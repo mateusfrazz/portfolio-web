5873a7a84a6242c85454468625e7a33a1aa802fe
mateusfrazz <mateusfrazao12345@gmail.com>
2024-11-11 15:12:35 -0300

    finalizado as funções para salvar e obter as informações

--- src/scripts/mensagens.js
+++ src/scripts/mensagens.js
@@ -7,10 +7,15 @@ if (window.location.pathname.includes("mensagens.html")) {
     carregar();
 }
 
+
+function buscarDados() {
+    return JSON.parse(localStorage.getItem("dadosSalvos")) || [];
+}
 // Função para salvar dados no localStorage
 function salvar() {
     // Cria o array e obtém os dados do localStorage convertendo novamente para o array
     var email = document.getElementById("dado1").value;
+    console.log('testando',document.getElementById("dado1"));
     var mensagem = document.getElementById("dado2").value;
 
     // Verificação para saber se todos os dados foram preenchidos
@@ -21,16 +26,13 @@ function salvar() {
         var dados = { email, mensagem };
 
         // Recupera o array existente no localStorage ou cria um novo
-        const dadosSalvos = JSON.parse(localStorage.getItem("dadosSalvos")) || [];
+        let dadosSalvos = buscarDados();
 
         // Transformando esse objeto para um array
         dadosSalvos.push(dados);
 
         // Armazena os dados no localStorage
         localStorage.setItem("dadosSalvos", JSON.stringify(dadosSalvos));
-
-        // Chama a função carregar para atualizar os cards
-        carregar();
 
     } else {
         alert("Por favor, preencha todos os campos");
@@ -40,40 +42,29 @@ function salvar() {
 // Função para carregar dados do localStorage
 function carregar() {
     const container = document.getElementById('containerCards');
-
+    
+    
     // Recupera os dados salvos no localStorage ou cria um array vazio
-    const dadosSalvos = JSON.parse(localStorage.getItem("dadosSalvos")) || [];
-
+    let dadosSalvos = buscarDados();
+     console.log(dadosSalvos);
     // Verifica se há dados salvos e os exibe
-    if (dadosSalvos && Array.isArray(dadosSalvos)) {
-        dadosSalvos.forEach(dado => {
-            console.log(dado);
-
+    if (dadosSalvos) {
+        dadosSalvos.forEach((dado,index) => {
             // Cria os cards
-            const card = document.createElement('div');
-            card.classList.add('card'); // Adicionando a classe para ser estilizada
-
-            const cardTitulo = document.createElement('h3');
-            cardTitulo.textContent = dado.email; // Exibe o email como título
-            card.classList.add('text')
-            card.appendChild(cardTitulo);
-
-            const cardDescricao = document.createElement('p');
-            cardDescricao.textContent = dado.mensagem; // Atribui a mensagem como descrição
-            card.classList.add('text')
-            card.appendChild(cardDescricao);
-             
-            //adicionando os botoes
-            const cardButton = document.createElement('button');
-            card.classList.add('buttons')
-            card.appendChild(cardButton);
-
-            // Adiciona o card ao contêiner
-            container.appendChild(card);
+            const card = `
+            <div class="card">
+              <h3 class="text">${dado.email}</h3>
+              <p class="text">${dado.mensagem}</p>
+              <button class="buttons" onclick='deletarItem(${index})'></button>
+            </div>
+            
+            `
+            container.innerHTML += card;
         });
     } else {
         alert('Erro, não há mensagens para mostrar no momento.');
     }
+    
 }
 
 // Função para limpar dados do localStorage
@@ -82,6 +73,9 @@ function limpar() {
     alert('Todos os itens foram excluídos');
 }
 
-function limparDadoUnico () {
-
+function deletarItem(index) {
+    const dadosSalvos = buscarDados();
+    dadosSalvos.splice(index, 1)
+    localStorage.setItem("dadosSalvos", JSON.stringify(dadosSalvos));
+    location.reload()
 }
